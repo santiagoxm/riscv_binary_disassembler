e9fab04f4f2cf43c598567a1c4805699cf2916b3
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-08-11 17:58:01 -0300

    working r-type

--- disassembler.c
+++ disassembler.c
@@ -3,7 +3,8 @@
 #include <unistd.h>
 #include <inttypes.h> 
 
-const char* regs[] = {"zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+char* regs[] = {"zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+char* rnames[] = {"add\0sub", "sll", "slt", "sltu", "xor", "srl\0sra", "or", "and"};
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
@@ -11,7 +12,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	const char* path = argv[1];
+	char* path = argv[1];
 		
 	uint32_t ins = 0;
 
@@ -22,8 +23,11 @@ int main(int argc, char* argv[]) {
 	uint8_t opcode;
 	uint8_t rd;
 	uint8_t funct3;
+	uint8_t rs1;
+	uint8_t rs2;
 	uint8_t funct7;
 	int32_t immj;
+	char* opname;
 
 	while (read(fd, (uint8_t*) &ins, 4) != 0) {
 		opcode = ins & 0x7F; // bits 0 to 6
@@ -31,11 +35,25 @@ int main(int argc, char* argv[]) {
 
 		switch (opcode) {
 		case 51: // R-type
-			printf("R-type\n");
 			funct3 = (ins & 0x7000) >> 12; // bits 12 to 14
+			rs1 = (ins & 0xf8000) >> 15; // bits 15 to 19
+			rs2 = (ins & 0x1f00000) >> 20; // bits 20 to 24
 			funct7 = (ins & 0xFC000000) >> 25; // bits 25 to 31
-			printf("funct3: %b\n", funct3);
-			printf("funct7: %b\n", funct3);
+
+			if (funct3 > 7) {
+				printf("0x%X: unknown funct3 %b\n", address, funct3);
+			}
+
+			if (funct7 != 0 && funct7 != 32) {
+				printf("0x%X: unknown funct7 %b\n", address, funct7);
+				break;
+			}
+
+			opname = rnames[funct3];
+
+			if (funct7 == 32) {opname += 4;}
+
+			printf("0x%X: %s, %s, %s, %s\n", address, opname, regs[rd], regs[rs1], regs[rs2]);
 
 			break;
 		case 3: // I-type
